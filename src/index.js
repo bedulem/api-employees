317b88b5e54927314d114bd74f7c59254cde2349
bedulem <santochibelen@gmail.com>
2023-01-08 19:14:49 -0300

    app efactor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import express from "express";
-import employeesRoutes from "./routes/employees.routes.js";
-import inedexRoutes from "./routes/index.routes.js";
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-
-app.use(inedexRoutes);
-app.use("/api", employeesRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: "endpoint not found",
-  });
-});
+import { PORT } from "./config.js";
+import app from "./app.js";
 
 app.listen(PORT);
 
