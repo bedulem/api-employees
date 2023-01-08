69ddfe615aeff5884a0a777d75e2ca29ad0fdb35
bedulem <santochibelen@gmail.com>
2023-01-08 18:52:27 -0300

    endpoint not found

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ app.use(express.json());
 app.use(inedexRoutes);
 app.use("/api", employeesRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "endpoint not found",
+  });
+});
+
 app.listen(PORT);
 
 console.log(`Server running on port ${PORT}`);
